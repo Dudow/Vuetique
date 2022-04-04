0609c17e0f39ff5ef5b93fe9a27ba44e02939cb4
Dudow <edu30.nm@gmail.com>
2022-04-04 09:07:56 -0300

    feat: make navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-drawer v-model="isOpen" show-if-above bordered class="bg-grey-2">
+    <q-list padding class="rounded-borders">
+      <q-item-label header>Pages</q-item-label>
+      <q-item
+        clickable
+        to="/"
+        exact
+        active-class="my-menu-link"
+        class="q-mx-lg rounded-borders q-mb-sm"
+      >
+        <q-item-section avatar>
+          <q-icon name="shopping_bag" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Home</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item
+        clickable
+        to="/cart"
+        exact
+        active-class="my-menu-link"
+        class="q-mx-lg rounded-borders"
+      >
+        <q-item-section avatar>
+          <q-icon name="shopping_cart" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Cart</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-drawer>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    isOpen: Boolean,
+  },
+};
+</script>
+
+<style>
+.my-menu-link {
+  color: white;
+  background: #f2c037;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Home />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Home from "../components/Home.vue";
 
-export default defineComponent({
-  name: 'HomeView',
+export default {
+  name: "HomeView",
   components: {
-    HelloWorld,
+    Home,
   },
-});
+};
 </script>
